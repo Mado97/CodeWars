1c57d481ee0f68f813fb1f64749424ae6af7e6f6
Omar Mado <90534539+Mado97@users.noreply.github.com>
2022-10-19 10:03:46 -0500

    Categorize Senior or Junior
    To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.

---
+++ 8Kyu/CategorizeMember.py
@@ -0,0 +1,8 @@
+def open_or_senior(data):
+    x = []
+    for i in range(0,len(data)):
+        if data[i][0] >= 55 and data[i][1] > 7:
+            x.append('Senior')
+        else: 
+            x.append('Open')
+    return x
