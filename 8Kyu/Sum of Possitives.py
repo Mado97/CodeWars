cf7ac07d9d89c03bd1c6f9bf06d1804c6ce4eb7e
Omar Mado <90534539+Mado97@users.noreply.github.com>
2022-10-19 09:46:45 -0500

    Sum of Possitives V 2.0

--- 8Kyu/Sum of Possitives.py
+++ 8Kyu/Sum of Possitives.py
@@ -1,7 +1,12 @@
 def positive_sum(arr):
+        
+    return sum(x for x in arr)
+"""
+def positive_sum(arr):
     x = []
     for i in arr:
         if i > 0:
             x.append(i)
         
     return sum(x)
+"""
