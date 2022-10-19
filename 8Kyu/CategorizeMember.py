cff6b8c733d3db12940130e2b7c67b32e8d2c909
Omar Mado <90534539+Mado97@users.noreply.github.com>
2022-10-19 10:04:59 -0500

    Senior or Junior V 2.0

--- 8Kyu/CategorizeMember.py
+++ 8Kyu/CategorizeMember.py
@@ -1,8 +1,20 @@
 def open_or_senior(data):
     x = []
+    for (i,j) in data:
+        if i >= 55 and j > 7:
+            x.append('Senior')
+        else: 
+            x.append('Open')    
+    return x
+
+"""
+VERSION 1
+def open_or_senior(data):
+    x = []
     for i in range(0,len(data)):
         if data[i][0] >= 55 and data[i][1] > 7:
             x.append('Senior')
         else: 
             x.append('Open')
     return x
+"""
